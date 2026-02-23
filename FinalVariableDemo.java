22697186fbf45ad3b1d3fb1e52c11530cd139c04
Mahalakshmi L <mahalakshmil6505@gmail.com>
2026-02-23 10:36:02 +0530

    Initial commit

---
+++ FinalVariableDemo.java
@@ -0,0 +1,14 @@
+package org.tnsif.acc.c2tc.finalkeyworddemo;
+
+public class FinalVariableDemo {
+
+	public static void main(String[] args) {
+		
+		final int num=8;
+		num=9;
+		System.out.println(num);
+
+	}
+
+}
+
